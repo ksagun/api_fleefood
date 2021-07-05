5ba091f5d2a9e9da62078f7d6ea1affeeb2754dd
ksagun <ksagun117@gmail.com>
2021-07-05 23:19:26 +0800

    FFS-4: Added UUID id masking

--- classes/uuid.php
+++ classes/uuid.php
@@ -1,7 +1,26 @@
 <?php 
 
-    
+class UUID{
+    private static $secret = "ffs-128-serve";
+
+    public static function encode($sid = null){
+        $s = base64_encode(self::$secret);
+        $id = base64_encode($sid);
+        $uuid = $s.'_'.$id;
+        $encoded = base64_encode($uuid);
+        return $encoded;
+    }
 
 
+    public static function decode($sid = null){
+        $encoded = base64_decode($sid);
+        $uuid = explode('_', $encoded);
+        $s = base64_decode($uuid[0]);
+
+        if($s == self::$secret){
+            return base64_decode($uuid[1]);
+        }
+    }
+}
 
 ?>
--- services/restaurants/restaurants.php
+++ services/restaurants/restaurants.php
@@ -1,12 +1,20 @@
 <?php 
 include_once "model/getRestaurants.php";
+require_once "../api/classes/uuid.php";
 require_once "../api/classes/cinput.php";
 
 class Restaurant{
     public function restaurantsController($location = null){
         $list = new RestaurantModel();
         $location = cinput::input($location);
-        echo json_encode($list->getRestaurantList($location));
+        $data = $list->getRestaurantList($location);
+
+        //Loop through data and encode id
+        foreach($data as &$row){
+            $row['id'] = UUID::encode($row['id']);
+        }
+
+        echo json_encode($data);
     }
 
 }
