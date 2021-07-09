d620d7ad766fd8d4f93a9ef3b063b1f907c89b51
ksagun <ksagun117@gmail.com>
2021-07-09 14:32:11 +0800

    FFS-4: Added error checking before encoding

--- services/restaurants/restaurants.php
+++ services/restaurants/restaurants.php
@@ -5,16 +5,19 @@ require_once "../api/classes/cinput.php";
 
 class Restaurant{
     public function restaurantsController($location = null){
+        echo $location;
         $uuid = new UUID();
         $list = new RestaurantModel();
         $location = cinput::input($location);
         $data = $list->getRestaurantList($location);
 
         //Loop through data and encode id
-        foreach($data as &$row){
-            $row['id'] = $uuid->encode($row['id']);
+        if($data["success"]){
+            foreach($data as &$row){
+                $row['id'] = $uuid->encode($row['id']);
+            }    
         }
-
+       
         echo json_encode($data);
     }
 
