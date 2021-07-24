c6e56f5fbc2c50cf703af4bc19226df8344c89e8
ksagun <ksagun117@gmail.com>
2021-07-25 00:38:35 +0800

    FF-8: Added restaurant details and merged menu items

--- services/restaurants/lib/queries.php
+++ services/restaurants/lib/queries.php
@@ -13,16 +13,34 @@ $GET_RESTAURANT_LIST_BY_LOCATION = 'SELECT
                                     FROM merchant 
                                     WHERE business_location LIKE :location';
 
+$GET_RESTAURANT_DETAILS = 'SELECT 
+                           m.id as "id",
+                           m.business_name as "restaurant",
+                           m.banner_image as "bannerImage",
+                           m.opening as "opening",
+                           m.closing as "closing",
+                           m.business_location as "location",
+                           m.address_line_1 as "address1",
+                           m.address_line_2 as "address2",
+                           m.province as "province",
+                           m.city as "city",
+                           m.municipality as "municipality",
+                           m.zip_code as "zip"
+                           FROM merchant m
+                           WHERE m.id = :id AND 
+                           m.business_name = :name';
+
+
 $GET_RESTAURANT_MENU = 'SELECT 
-                        m.business_name as "restaurant",
-                        m.business_location as "location",
                         mp.id as "itemId",
                         mp.product_name as "itemName",
                         mp.product_price as "itemPrice",
                         mp.in_stock as "inStock",
                         mp.is_available as "isAvailable",
-                        mc.category_name as "category"
+                        mc.category_name as "category",
+                        pi.image_path as "productImage"
                         FROM merchant m
                         INNER JOIN merchant_products mp ON merchant_id = m.id
                         INNER JOIN menu_category mc ON mc.id = mp.category_id
+                        INNER JOIN product_images pi ON pi.id = mp.product_image_id
                         WHERE m.business_name = :name AND m.id = :id';
