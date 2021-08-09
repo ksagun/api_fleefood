5e999f17d338490e841bfdc343337bf4f70eec46
ksagun <ksagun117@gmail.com>
2021-08-09 23:22:56 +0800

    No message

--- lib/http-config.php
+++ lib/http-config.php
@@ -3,7 +3,7 @@
 if (isset($_SERVER['HTTP_ORIGIN'])) {
      header("Access-Control-Allow-Origin: *");
      //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
      header("Content-Type: application/json; charset=UTF-8");
      header('Access-Control-Allow-Credentials: true');
      header("Access-Control-Max-Age: 3600");
