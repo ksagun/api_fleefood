c018a0b99020310a6260d2437b0546805124fe1e
ksagun <ksagun117@gmail.com>
2021-08-09 23:39:36 +0800

    No message

--- lib/http-config.php
+++ lib/http-config.php
@@ -1,11 +1,10 @@
 <?php
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-     header("Access-Control-Allow-Origin: *");
-     //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+     //header("Access-Control-Allow-Origin: *");
+     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
      header("Content-Type: application/json; charset=UTF-8");
-     header('Access-Control-Allow-Credentials: true');
+     header("Access-Control-Allow-Credentials: true");
      header("Access-Control-Max-Age: 3600");
 
      //COMMENT IF ALREADY TESTING WITH FRONTEND
